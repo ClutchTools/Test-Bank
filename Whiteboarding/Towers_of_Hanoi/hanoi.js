e2ee59af77476d55d0ffb04710f9ce1d5313c3bb
Ethan Lipkind <ethan.lipkind@gmail.com>
2018-02-22 19:35:25 -0500

    solution code

--- Whiteboarding/Towers_of_Hanoi/hanoi.js
+++ Whiteboarding/Towers_of_Hanoi/hanoi.js
@@ -2,7 +2,7 @@ let hanoi = (n, start, helper, destination) => {
     if ( n > 0 ) {
         // move tower of size n - 1 from start to helper 
         hanoi(n - 1, start, destination, helper);
-        // move disc from start to destination
+        // move disc from start to destination and print out the description of the move
         if ( start[0].length >= 0 ) {
             let disc = start[0].pop();
             console.log('moving disc ' + disc + ' from ' + start[1] + ' to ' + destination[1]);
