58c4bf52c7482c38ffa51eb617b626437c8ec3a5
Jason McCutchan <jason.mccutchan@uconn.edu>
2018-03-29 00:43:22 -0400

    Update SOLUTION.js

--- Whiteboarding/TwoSums/SOLUTION.js
+++ Whiteboarding/TwoSums/SOLUTION.js
@@ -1,4 +1,6 @@
-var twoSums = (array, target) => {
+
+//Version 1 (Constant Time)
+const twoSums = (array, target) => {
   var obj = {}
   for(var i=0;i<array.length;i++){
     if(obj[array[i]]){
@@ -6,6 +8,26 @@ var twoSums = (array, target) => {
     } else{
       obj[target-array[i]] = array[i]
     }
+  }
+  return false
+}
+
+//Version2 (Constant Space and Constant Time)
+const twoSums = (array, target) =>{
+  let A = 0
+  let B;
+  if(Array.isArray(array) && array.length > 0){
+    B = array.length-1
+  }else{
+    B = 0
+  }
+  while(A !== B)
+  if(array[A]+array[B] === target){
+    return [array[A],array[B]]
+  } else if(array[A]+array[B] > target){
+    B -= 1
+  } else if(array[A]+array[B] < target){
+    A += 1
   }
   return false
 }
