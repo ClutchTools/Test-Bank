d95700473558aa7338861043f122219d30c5e02b
Jason McCutchan <jasonmccutchan@Jasons-MBP.fios-router.home>
2018-06-28 21:49:36 -0400

    wierd git issue

---
+++ Whiteboarding/Childhood_Stairs/SOLUTION.js
@@ -0,0 +1,60 @@
+
+
+/* Brute Force Permutation */
+
+const child = (n, count) => {
+  count = count || 0
+  let result = 0
+  const steps = [1,2,3]
+  steps.forEach((val) => {
+    count += val
+    if(count < n){
+    result += child(n, count)
+    count -= val
+    } else if(count === n){
+      result += 1
+    } else{
+      count -= val
+    }
+  })
+  
+  return result
+  
+}
+
+/* Slightly better permutation/brute force */
+
+const child = (n, count) => {
+  count = count || 0
+  let result = 0
+  if(count <= n-3){
+    result += child(n, count+1)
+    result += child(n, count+2)
+    result += child(n, count+3)
+  } else if(count <= n-2){
+    result += child(n, count+1)
+    result += child(n, count+2)
+  } else if(count <= n-1){
+    result += child(n, count+1)
+  } else {
+    result += 1
+  }
+  return result
+  
+}
+
+
+/* Dyanmic Programming Solution *BEST SOLUTION* */
+
+const child = (steps, memo = []) => {
+  if (steps < 0) {
+    return 0;
+  } else if (steps === 0) {
+    return 1;
+  } else if (memo[steps] > -1) {
+    return memo[steps];
+  } else {
+    memo[steps] = child(steps - 1, memo) + child(steps - 2, memo) + child(steps - 3, memo);
+    return memo[steps];
+  }
+}
---
+++ Whiteboarding/Permutations_I/README.md
@@ -0,0 +1,20 @@
+# Permutations I
+## Prompt
+
+Given an array of unique integers, return an array of all possible permutations.<br>
+
+
+Example to give
+```
+Input: [1, 2, 3]
+Output: [
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+
+```
