e5b475734c2d3daf626bab00c9d09f25a473af81
Jason McCutchan <jason.mccutchan@uconn.edu>
2018-07-10 23:52:59 -0400

    Update SOLUTION.js

--- Whiteboarding/Childhood_Stairs/SOLUTION.js
+++ Whiteboarding/Childhood_Stairs/SOLUTION.js
@@ -44,7 +44,7 @@ const child = (n, count) => {
 }
 
 
-/* Dyanmic Programming Solution *BEST SOLUTION* */
+/* Dyanmic Programming Solution */
 
 const child = (steps, memo = []) => {
   if (steps < 0) {
@@ -57,4 +57,15 @@ const child = (steps, memo = []) => {
     memo[steps] = child(steps - 1, memo) + child(steps - 2, memo) + child(steps - 3, memo);
     return memo[steps];
   }
+}
+
+/* Best Solution but for specific reasons... can you figure out why it's better? and also why this solution is possible? */
+
+const childhoodStairs = function(n) {
+  let steps = [0,1,1]
+  while (n > 2) {
+    steps.push(steps.shift() + steps[0] + steps[1]) 
+    n--
+  }
+  return steps[0] + steps[1] + steps[2]
 }
